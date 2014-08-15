61ec809fa349c29611645d8914a484181043fdf9
Chris Graham <chris.graham@blackboard.com>
2014-08-15 15:04:57 -0500

    more stuff

--- recipes/proxy_source.rb
+++ recipes/proxy_source.rb
@@ -24,7 +24,7 @@ when 'ubuntu', 'debian'
     php_packages = %w(php-pear php-dev)
     packages.push(*php_packages)
   end
-  init_template = 'zabbix_server.init.erb'
+  init_template = 'zabbix_proxy.init.erb'
 when 'redhat', 'centos', 'scientific', 'amazon', 'oracle'
   include_recipe 'yum-epel'
 
@@ -55,7 +55,7 @@ when 'redhat', 'centos', 'scientific', 'amazon', 'oracle'
     php_packages = %w(php-pear php-devel)
     packages.push(*php_packages)
   end
-  init_template = 'zabbix_server.init-rh.erb'
+  init_template = 'zabbix_proxy.init-rh.erb'
 end
 
 packages.each do |pck|
@@ -71,8 +71,7 @@ php_pear 'oci8' do
   only_if { node['zabbix']['database']['install_method'] == 'oracle' }
 end
 
-configure_options = node['zabbix']['server']['configure_options'].dup
-configure_options << "--enable-proxy" unless configure_options.include?("--enable-proxy")
+configure_options = node['zabbix']['proxy']['configure_options'].dup
 configure_options = (configure_options || Array.new).delete_if do |option|
   option.match(/\s*--prefix(\s|=).+/)
 end
@@ -94,20 +93,20 @@ when 'oracle'
   configure_options << with_oracle_include unless configure_options.include?(with_oracle_include)
 end
 
-node.normal['zabbix']['server']['configure_options'] = configure_options
+node.normal['zabbix']['proxy']['configure_options'] = configure_options
 
-zabbix_source 'install_zabbix_server' do
-  branch node['zabbix']['server']['branch']
-  version node['zabbix']['server']['version']
-  source_url node['zabbix']['server']['source_url']
-  branch node['zabbix']['server']['branch']
-  version node['zabbix']['server']['version']
+zabbix_source 'install_zabbix_proxy' do
+  branch node['zabbix']['proxy']['branch']
+  version node['zabbix']['proxy']['version']
+  source_url node['zabbix']['proxy']['source_url']
+  branch node['zabbix']['proxy']['branch']
+  version node['zabbix']['proxy']['version']
   code_dir node['zabbix']['src_dir']
-  target_dir "zabbix-#{node['zabbix']['server']['version']}"
+  target_dir "zabbix-#{node['zabbix']['proxy']['version']}"
   install_dir node['zabbix']['install_dir']
   configure_options configure_options.join(' ')
 
-  action :install_server
+  action :install_proxy
 end
 
 # Install Init script
@@ -119,9 +118,9 @@ template '/etc/init.d/zabbix_proxy' do
   notifies :restart, 'service[zabbix_proxy]', :delayed
 end
 
-# install zabbix server conf
-template "#{node['zabbix']['etc_dir']}/zabbix_server.conf" do
-  source 'zabbix_server.conf.erb'
+# install zabbix proxy conf
+template "#{node['zabbix']['etc_dir']}/zabbix_proxy.conf" do
+  source 'zabbix_proxy.conf.erb'
   owner 'root'
   group 'root'
   mode '644'
@@ -131,14 +130,11 @@ template "#{node['zabbix']['etc_dir']}/zabbix_server.conf" do
     :dbuser             => node['zabbix']['database']['dbuser'],
     :dbpassword         => node['zabbix']['database']['dbpassword'],
     :dbport             => node['zabbix']['database']['dbport'],
-    :java_gateway       => node['zabbix']['server']['java_gateway'],
-    :java_gateway_port  => node['zabbix']['server']['java_gateway_port'],
-    :java_pollers       => node['zabbix']['server']['java_pollers']
   )
-  notifies :restart, 'service[zabbix_server]', :delayed
+  notifies :restart, 'service[zabbix_proxy]', :delayed
 end
 
-# Define zabbix_server service
+# Define zabbix_proxy service
 service 'zabbix_proxy' do
   supports :status => true, :start => true, :stop => true, :restart => true
     action [:start, :enable]
