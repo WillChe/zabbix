99e0bcca6aa80fd12e01d446fd479cb09d299e59
Chris Graham <chris.graham@blackboard.com>
2014-10-15 14:18:15 -0500

    adding php-ldap to centos packages

--- attributes/web.rb
+++ attributes/web.rb
@@ -19,8 +19,8 @@ default['zabbix']['web']['packages'] = value_for_platform_family(
   'debian' => %w(php5-mysql php5-gd libapache2-mod-php5),
   'rhel' =>
     if node['platform_version'].to_f < 6.0
-      %w(php53-mysql php53-gd php53-bcmath php53-mbstring)
+      %w(php53-mysql php53-gd php53-bcmath php53-mbstring php53-ldap)
     else
-      %w(php php-mysql php-gd php-bcmath php-mbstring php-xml)
+      %w(php php-mysql php-gd php-bcmath php-mbstring php-xml php-ldap)
     end
   )
