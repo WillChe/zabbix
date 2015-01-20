6bebb1216bd57da122fa1c3c961ba4c1cb8e4c4e
Chris Graham <chris.graham@blackboard.com>
2015-01-20 15:04:24 -0600

    alskdfjalsdj

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,6 +1,5 @@
 default['zabbix']['database']['install_method']     = 'mysql'
 default['mysql']['version'] = '5.6'
-default['mysql']['server_root_password'] = 'D8BTHxxLTze4Ig0DrWv6'
 default['zabbix']['database']['dbname']             = 'zabbix'
 default['zabbix']['database']['dbuser']             = 'zabbix'
 default['zabbix']['database']['dbhost']             = 'localhost'
