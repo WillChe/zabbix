c088e2753d2833a2e88fc10ed5fdf5f89fc02d49
Chris Graham <chris.graham@blackboard.com>
2015-11-10 12:58:27 -0600

    installing chef gem "mysql" to avoid require "mysql" errors

--- recipes/database.rb
+++ recipes/database.rb
@@ -1,5 +1,10 @@
 include_recipe 'zabbix::common'
 
+#including this because it seems to be necessary
+chef_gem 'mysql' do
+  action :install
+end
+
 ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 
 include_recipe 'database::mysql'
