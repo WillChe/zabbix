eb77f71159d66748f9c0793d2fb07090fb08bbce
Ilya Cherkasov <i.cherkasov@gmail.com>
2015-12-18 18:00:05 +0600

    fix webapi url add missing php5-pgsql for debian platform

--- attributes/web.rb
+++ attributes/web.rb
@@ -2,7 +2,7 @@ default['zabbix']['web']['login'] = 'admin'
 default['zabbix']['web']['password'] = 'zabbix'
 default['zabbix']['web']['install_method']  = 'apache'
 default['zabbix']['web']['fqdn']            = node['fqdn']
-default['zabbix']['web']['api']['uri'] = '/api_jasonrpc.php'
+default['zabbix']['web']['api']['uri'] = '/api_jsonrpc.php'
 default['zabbix']['web']['api']['scheme']   = 'http'
 default['zabbix']['web']['aliases']         = ['zabbix']
 default['zabbix']['web']['port']            = ['80']
@@ -18,7 +18,7 @@ default['zabbix']['web']['php']['settings']    = {
 }
 
 default['zabbix']['web']['packages'] = value_for_platform_family(
-  'debian' => %w(php5-mysql php5-gd libapache2-mod-php5),
+  'debian' => %w(php5-mysql php5-gd libapache2-mod-php5 php5-pgsql),
   'rhel' =>
     if node['platform_version'].to_f < 6.0
       %w(php53-mysql php53-gd php53-bcmath php53-mbstring php53-ldap)
